904eb8b0d710b75fc5f7b2fa1e33adbdf88aeff3
softrebel <sh.mohammad66@gmail.com>
2021-11-05 15:26:06 +0330

    change plot save svg

--- igraphdiagram.py
+++ igraphdiagram.py
@@ -3,4 +3,4 @@ from igraph import *
 g = Graph.Read_GraphML("zachary.graphml")
 #same as using layout = g.layout("kk")
 layout = g.layout_kamada_kawai()
-plot(g, layout=layout).save('igraphdiagram.png')
+plot(g,'igraphdiagram.svg', layout=layout)
